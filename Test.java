a830551ea15484787979093cd33850635b403199
aayushmishra321 <Maayush081@gmail.com>
2024-10-19 16:44:39 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 public class Test{
     public static void main(String[]args){
-      // int a = 5;
-      // int b = 54;
-      System.out.println("Addition of "+args[0]+" & "+args[1]+" is "+ (Integer.parseInt(args[0])+Integer.parseInt(args[1])));
-    }
+      int num1 = Integer.parseInt(args[0]);
+      int num2 = Integer.parseInt(args[1]);
+      System.out.println("Addition of "+ num1 +" & "+ num2 +" is "+(num1 + num2));
+    }
 }
-
